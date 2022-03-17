53ea0d77e55683834ae1606fcd1675dc3d7e6f42
Caio <caioqs.info@gmail.com>
2022-03-17 12:26:28 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+    try:
+        with open(path_file, "r") as file:
+            file_data = file.read().split("\n")
+            return file_data
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
