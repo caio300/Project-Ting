6f64b5fdae788ea8431cf9b4f58b08a18e142720
Caio <caioqs.info@gmail.com>
2022-03-17 16:34:27 -0300

    Req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = []
+
+    current_position = instance.search(0)
+
+    for index, line in enumerate(current_position["linhas_do_arquivo"]):
+        if word in line:
+            output.append({
+                "palavra": word,
+                "arquivo": current_position["nome_do_arquivo"],
+                "ocorrencias": [{"linha": index + 1}],
+            })
+
+    instance.dequeue()
+    return output
 
 
 def search_by_word(word, instance):
